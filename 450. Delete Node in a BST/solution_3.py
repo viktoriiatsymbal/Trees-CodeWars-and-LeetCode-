159bd608e523f4203359ae765575b3d0b187bf0f
viktoriiatsymbal <149361278+viktoriiatsymbal@users.noreply.github.com>
2024-04-20 17:06:47 +0300

    Created function to delete node and replace it with min from right subtree

---
+++ 450. Delete Node in a BST/solution_3.py
@@ -0,0 +1,34 @@
+"""
+Delete Node in a BST."""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    """
+    Solution class."""
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        """
+        Deletes node and replaces its value with minimum value of the right subtree.
+        """
+        if not root:
+            return root
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            min_node = root.right
+            while min_node.left:
+                min_node = min_node.left
+            root.val = min_node.val
+            root.right = self.deleteNode(root.right, min_node.val)
+        return root
